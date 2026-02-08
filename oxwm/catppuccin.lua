ab57e44b712d56f0eb203209c28bbcc9dcff9978
TheIdioticDev <168137332+theidioticdev@users.noreply.github.com>
2026-02-08 09:19:33 +0200

    Add files via upload

---
+++ oxwm/catppuccin.lua
@@ -0,0 +1,13 @@
+return {
+	fg = "#cdd6f4",
+	red = "#f38ba8",
+	bg = "#1e1e2e",
+	cyan = "#94e2d5",
+	green = "#a6e3a1",
+	lavender = "#b4befe",
+	light_blue = "#89dceb",
+	grey = "#9399b2",
+	blue = "#89b4fa",
+	purple = "#cba6f7",
+	sep = "#45475a",
+}
